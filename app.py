016d77891e6101c721ed4be68ac876dba4e8a943
mattw44 <supik713@gmail.com>
2025-05-29 22:19:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title="Aplikasi Tingkat Stres", layout="centered")
 st.sidebar.header("Main Menu")
 
-st.title("Kabeh Berhak Bahagia, You with person anyarmu, awakku mbek mie ayam langananku")
-st.image ("https://cdn.idntimes.com/content-images/community/2024/07/img-20240701-201002-8f5a223bff1780cec432ec94722b7950_600x400.jpg")
+st.title("APPLIKASI MENGUKUR TINGKAT STRES")
+st.image ("https://dinkes.bandaacehkota.go.id/wp-content/uploads/sites/36/2025/02/stress.png")
