5abb872fa9a206e35f2e58cb3b87e972af60c353
mattw44 <supik713@gmail.com>
2025-05-29 13:33:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.set_page_config(page_title="Aplikasi Multi-Halaman", layout="centered")
+st.set_page_config(page_title="Aplikasi Tingkat Stres", layout="centered")
+st.sidebar.header("Dashboard")
 
-st.title("Selamat Datang di Aplikasi Streamlit")
-st.write("Gunakan menu di sebelah kiri untuk navigasi antar halaman.")
+st.title("Selamat datang pada di aplikasi Pengukur Tingkat Stres Mahasiswa")
+st.writer("Aplikasi ini dibuat untuk mengukur tingkat stres mahasiswa berdasarkan gaya hidup")
