224847c5daa4aacc76c28f113d200d43c06a5261
mattw44 <supik713@gmail.com>
2025-05-29 15:23:42 +0700

    Update 3_Prediction.py

--- pages/3_Prediction.py
+++ pages/3_Prediction.py
@@ -4,4 +4,5 @@ import pandas as pd
 st.set_page_config(page_title="Aplikasi Tingkat Stres", layout="centered")
 st.sidebar.header("Prediction")
 
-
+st.title("Ayo Ukur Tingkat Stres Kalian")
+st.write("Masukkan data-data yang diperlukan dalam kolom-kolom berikut")
