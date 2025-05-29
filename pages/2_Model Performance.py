c1446525d7c70d02526f53de2df787e13423ac08
mattw44 <supik713@gmail.com>
2025-05-29 15:35:36 +0700

    Update 2_Model Performance.py

--- pages/2_Model Performance.py
+++ pages/2_Model Performance.py
@@ -3,3 +3,6 @@ import pandas as pd
 
 st.set_page_config(page_title="Aplikasi Tingkat Stres", layout="centered")
 st.sidebar.header("Model Performance")
+
+st.title("Model Performa")
+st.writer("Pilih model performa yang kalian inginkan:)
