4503d67aaed1e9e40f0a1b18b72dc25c9ec868a5
mattw44 <supik713@gmail.com>
2025-05-29 19:03:55 +0700

    Update 3_Prediction.py

--- pages/3_Prediction.py
+++ pages/3_Prediction.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import pandas as pd
+import joblib
 
 st.set_page_config(page_title="Aplikasi Tingkat Stres", layout="centered")
 st.sidebar.header("Prediction")
 
 st.title("Ayo Ukur Tingkat Stres Kalian")
 st.write("Masukkan data-data yang diperlukan dalam kolom-kolom berikut")
+
+st.cache_resource 
+def load_model(path):
+  model=joblib.load(path)
+  return model
+
+model = load_model('model/decision_tree_model.pkl')
