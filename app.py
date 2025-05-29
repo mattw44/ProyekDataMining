cbbfa92fc545b1c2f1d00c1fb10085e8066d47a2
mattw44 <supik713@gmail.com>
2025-05-29 21:59:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.set_page_config(page_title="Aplikasi Tingkat Stres", layout="centered")
 st.sidebar.header("Main Menu")
 
 st.title("Kabeh Berhak Bahagia, You with person anyarmu, awakku mbek mie ayam langananku")
-st.image ("https://cdn.idntimes.com/content-images/community/2024/07/img-20240701-201002-8f5a223bff1780cec432ec94722b7950_600x400.jpg", caption=" JAWA!", use_column_width=True)
+st.image ("https://cdn.idntimes.com/content-images/community/2024/07/img-20240701-201002-8f5a223bff1780cec432ec94722b7950_600x400.jpg")
