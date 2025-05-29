3d7b10c48f8922357cdcfb06d7e9a4def6a820ee
mattw44 <supik713@gmail.com>
2025-05-29 15:38:13 +0700

    Update 2_Model Performance.py

--- pages/2_Model Performance.py
+++ pages/2_Model Performance.py
@@ -5,4 +5,4 @@ st.set_page_config(page_title="Aplikasi Tingkat Stres", layout="centered")
 st.sidebar.header("Model Performance")
 
 st.title("Model Performa")
-st.writer("Pilih model performa yang kalian inginkan")
+st.write("Pilih model performa yang kalian inginkan")
