6b5bc1973f757373ef3fad080640dfe63018fe5b
mattw44 <supik713@gmail.com>
2025-05-29 19:18:56 +0700

    Update 3_Prediction.py

--- pages/3_Prediction.py
+++ pages/3_Prediction.py
@@ -8,22 +8,31 @@ st.sidebar.header("Prediction")
 st.title("Ayo Ukur Tingkat Stres Kalian")
 st.write("Masukkan data-data yang diperlukan dalam kolom-kolom berikut")
 
-st.cache_resource 
+@st.cache_resource  # Tambahkan tanda @ untuk decorator
 def load_model(path):
-  model=joblib.load(path)
-  return model
+    model = joblib.load(path)
+    return model
 
 model = load_model('model/decision_tree_model.pkl')
 
-shpd = st.number_input("Study_Hours_Per_Day",min_value=0.0, max_value=10.0, value=2.0")
-ehpd = st.number_input("Extracurricular_Hours_Per_Day",min_value=0.0, max_value=10.0, value=2.0")
-s2hpd = st.number_input("Sleep_Hours_Per_Day",min_value=0.0, max_value=8.0, value=2.0")
-s3hpd = st.number_input("Social_Hours_Per_Day",min_value=0.0, max_value=10.0, value=2.0")
-pahpd = st.number_input("Physical_Activity_Hours_Per_Day",min_value=0.0, max_value=8.0, value=2.0")
-grades = st.number_input("Grades",min_value=0.0, max_value=13.0, value=2.0")
+shpd = st.number_input("Study_Hours_Per_Day", min_value=0.0, max_value=10.0, value=2.0)  # hapus tanda " setelah angka default
+ehpd = st.number_input("Extracurricular_Hours_Per_Day", min_value=0.0, max_value=10.0, value=2.0)
+s2hpd = st.number_input("Sleep_Hours_Per_Day", min_value=0.0, max_value=8.0, value=2.0)
+s3hpd = st.number_input("Social_Hours_Per_Day", min_value=0.0, max_value=10.0, value=2.0)
+pahpd = st.number_input("Physical_Activity_Hours_Per_Day", min_value=0.0, max_value=8.0, value=2.0)
+grades = st.number_input("Grades", min_value=0.0, max_value=13.0, value=2.0)
 
-if st.button("Prediksi")
-    input_data = pd.DataFrame([[shpd,ehpd,s2hpd,s3hpd,pahpd,grades]],
-                              columns=["Study_Hours_Per_Day","Extracurricular_Hours_Per_Day", "Sleep_Hours_Per_Day","Social_Hours_Per_Day","Physical_Activity_Hours_Per_Day","Grades"])
+if st.button("Prediksi"):  # tambahkan tanda titik dua ':'
+    input_data = pd.DataFrame(
+        [[shpd, ehpd, s2hpd, s3hpd, pahpd, grades]],
+        columns=[
+            "Study_Hours_Per_Day",
+            "Extracurricular_Hours_Per_Day",
+            "Sleep_Hours_Per_Day",
+            "Social_Hours_Per_Day",
+            "Physical_Activity_Hours_Per_Day",
+            "Grades"
+        ]
+    )
     hasil = model.predict(input_data)
-    st.success(f"Hasil Prediksi:{hasil[0]}")
+    st.success(f"Hasil Prediksi: {hasil[0]}")
