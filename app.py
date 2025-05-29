4bc82ba544440b502e813941ce0755e3f88073c6
mattw44 <supik713@gmail.com>
2025-05-29 13:56:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,8 @@ st.title("Selamat datang pada di aplikasi Pengukur Tingkat Stres Mahasiswa")
 st.write("Aplikasi ini dibuat untuk mengukur tingkat stres mahasiswa berdasarkan gaya hidup.")
 
 #Load Dataset
-df.pd.read_csv(
+df=pd.read_csv("model/Data Student lifestyle.csv")
+
+#Tampilan Data Frame
+st.subheader("Dataset Tingkat Stres Mahasiswa")
+st.dataframe(df)
