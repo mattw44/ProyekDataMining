0ed670b45c762c95096363ee7e07dc3bc736734d
mattw44 <supik713@gmail.com>
2025-05-30 07:21:02 +0700

    Update 2_Model Performance.py

--- pages/2_Model Performance.py
+++ pages/2_Model Performance.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import calssification_report, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score
 
 st.set_page_config(page_title="Aplikasi Tingkat Stres", layout="centered")
 st.sidebar.header("Model Performance")
@@ -12,9 +12,12 @@ st.write("Pilih model performa yang kalian inginkan")
 
 df = pd.read_csv("model/Data Student lifestyle.csv")
 
-testing = st.slider("Data Testing", min value=10, max_value=90, value=20)
-st.write(f"Nilai yang dipilih: {testing})
+testing = st.slider("Data Testing", min_value=10, max_value=90, value=20)
+st.write(f"Nilai yang dipilih: {testing}")
 t_size = testing/100
+
+X = df.drop(columns=['Stress Level'])
+y = df['Stress Level']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=t_size, random_state=42)
 
