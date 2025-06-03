bc36f795f56925029d05697960dc8f069b5f00c2
mattw44 <supik713@gmail.com>
2025-06-03 13:03:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title="Aplikasi Tingkat Stres", layout="centered")
 st.sidebar.header("Main Menu")
 
+#Memberikan Judul dan Gambar yang menarik
 st.title("APPLIKASI MENGUKUR TINGKAT STRES")
 st.image ("https://dinkes.bandaacehkota.go.id/wp-content/uploads/sites/36/2025/02/stress.png")
 st.write ("Oleh Kelompok 16")
