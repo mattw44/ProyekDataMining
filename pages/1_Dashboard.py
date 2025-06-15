f3d152daeda0152470bd0dab09d2a4e8ccc924b1
mattw44 <supik713@gmail.com>
2025-06-15 12:29:07 +0700

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -6,7 +6,6 @@ st.set_page_config(page_title="Aplikasi Tingkat Stres", layout="centered")
 st.sidebar.header("Dashboard")
 
 st.title("Dashboard Aplikasi Pengukur Tingkat Stres Mahasiswa")
-st.write("Aplikasi ini dibuat untuk mengukur tingkat stres mahasiswa berdasarkan gaya hidup.")
 
 #Load Dataset
 df = pd.read_csv("model/Data Student lifestyle.csv", sep=';')
