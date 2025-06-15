e62e2d64066b87d2fb844c8eb3dbea3d3ff315fd
mattw44 <supik713@gmail.com>
2025-06-15 12:52:26 +0700

    Update 3_Prediction.py

--- pages/3_Prediction.py
+++ pages/3_Prediction.py
@@ -19,12 +19,12 @@ model = load_model('model/decision_tree_model.pkl')
 
 #Mendefinisikan variabel input
 nama = st.text_input("Masukkan nama Anda:")
-shpd = st.slider("Study_Hours_Per_Day", min_value=0.0, max_value=10.0, value=2.0)  # hapus tanda " setelah angka default
-ehpd = st.slider("Extracurricular_Hours_Per_Day", min_value=0.0, max_value=10.0, value=2.0)
-s2hpd = st.slider("Sleep_Hours_Per_Day", min_value=0.0, max_value=10.0, value=2.0)
-s3hpd = st.slider("Social_Hours_Per_Day", min_value=0.0, max_value=10.0, value=2.0)
-pahpd = st.slider("Physical_Activity_Hours_Per_Day", min_value=0.0, max_value=10.0, value=2.0)
-grades = st.slider("Grades", min_value=0.0, max_value=10.0, value=2.0)
+shpd = st.slider("Study Hours Per Day", min_value=0.0, max_value=10.0, value=2.0)  # hapus tanda " setelah angka default
+ehpd = st.slider("Extracurricular Hours Per Day", min_value=0.0, max_value=10.0, value=2.0)
+s2hpd = st.slider("Sleep Hours Per Day", min_value=0.0, max_value=10.0, value=2.0)
+s3hpd = st.slider("Social Hours Per Day", min_value=0.0, max_value=10.0, value=2.0)
+pahpd = st.slider("Physical Activity Hours Per Day", min_value=0.0, max_value=13.0, value=2.0)
+grades = st.slider("Grades (IPK*2,5)", min_value=0.0, max_value=10.0, value=2.0)
 
 #Menampilkan hasil prediksi 
 if st.button("Prediksi"):  
